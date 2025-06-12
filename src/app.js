12994c1e48c6ad3913228c6aba76198e1eab81e9
rushikesh <rushikesh.pawarms@gmail.com>
2025-06-12 16:17:29 +0530

    update , delete, get , create

--- src/app.js
+++ src/app.js
@@ -20,6 +20,60 @@ app.post('/signup', async (req, res) => {
     res.status(500).send("data not saved in Db,Something went wrong")
   }
 })
+//get user by email
+app.get('/user', async (req, res) => {
+  try {
+    const email = req.body.email
+    const firstName = req.body.firstName
+    // const user = await User.find({ email: email })
+    //const user = await User.find({firstName})
+    //const user = await User.find({})
+    const user = await User.findOne({ email: email })
+    if (user) {
+      // if (user.length > 0) {
+      res.send(user)
+    }
+    else {
+      res.send("User not found")
+    }
+  }
+  catch (err) {
+    res.send("Somthing went wrong")
+  }
+})
+
+//delete record by Id
+app.delete('/user', async (req, res) => {
+  try {
+    const userId = req.body.userId
+    const user = await User.findByIdAndDelete(userId)
+    console.log(user)
+    res.send("user deleted")
+  }
+  catch (err) {
+    res.status(500).send("Somthing went wrong")
+  }
+})
+
+//update record by Id
+
+app.patch('/user', async (req, res) => {
+  try {
+    const userId = req.body.userId
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const data = {
+      firstName, lastName
+    }
+    const user = await User.findByIdAndUpdate(userId, data, { returnDocument: "after" })
+    console.log(user)
+    res.send("user updated successfully")
+  }
+  catch (err) {
+    res.status(500).send("Somthing went wrong")
+  }
+})
+
 
 connectDb()
   .then(() => {
